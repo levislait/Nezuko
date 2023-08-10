1f5a68a95025a3b0d3e1882dd1c4470fa3aba2ba
levislait <130797634+levislait@users.noreply.github.com>
2023-08-10 11:17:04 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983, 923087880256";
+  gg = "2250545752448, 2250545752448";
 }
 
 global.owner = gg.split(",");
